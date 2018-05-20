e2de223b5642c8066974d1e5866da1d706ec3c02
Tashley <darthcoders2018@gmail.com>
2018-05-20 21:11:22 +0400

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,75 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app :clipped-left="clipped">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import HelloWorld from './components/HelloWorld';
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+        },
+        {
+          title: 'Home',
+        },
+        {
+          title: 'Profile',
+        },
+        {
+          title: 'Resume',
+        },
+        {
+          title: 'Achievements',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      title: 'CodenameTashley',
+    };
+  },
+};
+</script>
